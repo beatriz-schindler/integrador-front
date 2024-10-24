7c0555ff6d6898b7f1aa1083ff3a4e235f968504
Davi Sobreira <133677392+davisobreira22@users.noreply.github.com>
2024-10-24 20:18:23 -0300

    primeiras mudancas no css
    ta ficando bonito part.1

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, inject } from '@angular/core';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Login } from '../../models/login';
 import { Usuarios } from '../../models/usuarios';
 import { Router } from '@angular/router';
 import { UsuarioService } from '../../services/usuario-service';
 import { FormsModule } from '@angular/forms';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -14,67 +14,82 @@ import Swal from 'sweetalert2'
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  @ViewChild('loginVideo', { static: true }) loginVideo!: ElementRef<HTMLVideoElement>;
 
   login: Login = new Login();
   usuario: Usuarios = new Usuarios();
   router = inject(Router);
-
   usuarioService = inject(UsuarioService);
 
-  autenticar(){
-    this.findByLogin(this.login.usuario);
-
-    // Verifica se o usuário foi encontrado e compara a senha
-    if (this.usuario && this.usuario.senha === this.login.senha) {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.onmouseenter = Swal.stopTimer;
-          toast.onmouseleave = Swal.resumeTimer;
-        }
-      });
-      Toast.fire({
-        icon: "success",
-        title: "Autenticado com sucesso!"
-      });
-      this.router.navigate(['admin/dashboard']);
-      // Se o login e a senha estiverem corretos, redireciona para a página principal
-      this.router.navigate(['admin/dashboard']);
-    } else {
-      // Caso contrário, exibe uma mensagem de erro
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.onmouseenter = Swal.stopTimer;
-          toast.onmouseleave = Swal.resumeTimer;
-        }
-      });
-      Toast.fire({
-        icon: "error",
-        title: "Usuário ou senha incorretos!"
-      });
-    }
+  ngOnInit() {
+    this.tryPlayVideo();
   }
 
-  findByLogin(login: string){
+  tryPlayVideo() {
+    const video = this.loginVideo.nativeElement;
+
+    // Força o vídeo a estar em 'muted' para permitir autoplay
+    video.muted = true;
+
+    video.play().catch(error => {
+      console.error("Erro ao tentar reproduzir o vídeo:", error);
+
+      // Listener para interação do usuário se o autoplay falhar
+      document.addEventListener('click', () => {
+        video.play();
+      }, { once: true });
+    });
+  }
+
+  autenticar() {
+    this.findByLogin(this.login.usuario);
+  }
+
+  findByLogin(login: string) {
     this.usuarioService.findByLogin(login).subscribe({
       next: user => {
         this.usuario = user;
+
+        if (this.usuario && this.usuario.senha === this.login.senha) {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "bottom-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Autenticado com sucesso!"
+          });
+          this.router.navigate(['admin/dashboard']);
+        } else {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Usuário ou senha incorretos!"
+          });
+        }
       },
       error: erro => {
         alert('Deu erro');
       }
-    })
-
+    });
   }
-
 }
