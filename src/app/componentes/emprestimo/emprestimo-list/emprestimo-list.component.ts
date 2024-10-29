dbe62814edc61d0e1d641eb46f44566136e0024c
joaosagrath <joaosagrath@hotail.com>
2024-10-29 07:40:13 -0300

    Adicionado Relatorios e Graficos

--- src/app/componentes/emprestimo/emprestimo-list/emprestimo-list.component.ts
+++ src/app/componentes/emprestimo/emprestimo-list/emprestimo-list.component.ts
@@ -5,36 +5,52 @@ import { Emprestimos } from '../../../models/emprestimos';
 import { EmprestimoService } from '../../../services/emprestimo-service';
 import Swal from 'sweetalert2';
 import { DatePipe } from '@angular/common';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
-  selector: 'app-emprestimo-list',
-  standalone: true,
-  imports: [FormsModule, MdbFormsModule, DatePipe],
-  templateUrl: './emprestimo-list.component.html',
-  styleUrl: './emprestimo-list.component.scss'
+	selector: 'app-emprestimo-list',
+	standalone: true,
+	imports: [FormsModule, MdbFormsModule, DatePipe],
+	templateUrl: './emprestimo-list.component.html',
+	styleUrl: './emprestimo-list.component.scss',
 })
 export class EmprestimoListComponent {
+	lista: Emprestimos[] = [];
+	emprestimo!: Emprestimos;
 
-  lista: Emprestimos[] = [];
-  emprestimo!: Emprestimos;
+	emprestimoService = inject(EmprestimoService);
 
-  emprestimoService = inject(EmprestimoService);
+	constructor() {
+		this.findAll();
+	}
 
-  constructor(){
-    this.findAll();
-  }
+	findAll() {
+		this.emprestimoService.findAll().subscribe({
+			next: (list) => {
+				//Equivalente ao TRy
+				this.lista = list;
+			},
+			error: (erro) => {
+				// Equivalente ao CATCH ou EXCEPTIONS
+				Swal.fire('Erro', erro.error, 'error');
+			},
+		});
+	}
 
-  findAll(){
+	// Função para gerar o PDF
+	imprimirRelatorio() {
+		const element = document.getElementById('pdfContent');
+		const options = {
+			margin: 0.3,
+			filename: 'relatorio.pdf',
+			image: { type: 'jpeg', quality: 0.98 },
+			html2canvas: { scale: 1, scrollY: 0 },
+			jsPDF: { unit: 'cm', format: 'A4', orientation: 'portrait' },
+		};
 
-    this.emprestimoService.findAll().subscribe({
-        next: list => { //Equivalente ao TRy
-            this.lista = list;
-        },
-        error: erro => { // Equivalente ao CATCH ou EXCEPTIONS
-          Swal.fire("Erro", erro.error, 'error');
-        }
-    })
-  }
-
-
+		if (element) {
+			// Use html2pdf como uma função global
+			(window as any).html2pdf().set(options).from(element).save();
+		}
+	}
 }
--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EmprestimoService } from '../../services/emprestimo-service';
+import { Emprestimos } from '../../models/emprestimos';
+import Swal from 'sweetalert2';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+	selector: 'app-dashboard',
+	standalone: true,
+	imports: [NgxChartsModule],
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+	lista: Emprestimos[] = [];
+	emprestimosPorPatrimonio: any[] = [];
 
+	view: [number, number] = [700, 400];
+	colorScheme = {
+		domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+	};
+
+	emprestimoService = inject(EmprestimoService);
+
+	constructor() {
+		this.findAll();
+	}
+
+	findAll() {
+		this.emprestimoService.findAll().subscribe({
+			next: (list) => {
+				this.lista = list;
+				this.processarEmprestimosPorPatrimonio();
+			},
+			error: (erro) => {
+				Swal.fire('Erro', erro.error, 'error');
+			},
+		});
+	}
+
+	processarEmprestimosPorPatrimonio() {
+		// Contagem de empréstimos por patrimônio do equipamento
+		const equipamentoCountMap = new Map<string, number>();
+
+		this.lista.forEach((emprestimo) => {
+			const patrimonio = emprestimo.equipamento.patrimonio;
+			if (equipamentoCountMap.has(patrimonio)) {
+				equipamentoCountMap.set(patrimonio, equipamentoCountMap.get(patrimonio)! + 1);
+			} else {
+				equipamentoCountMap.set(patrimonio, 1);
+			}
+		});
+
+		// Convertendo o Map para o formato necessário pelo ngx-charts
+		this.emprestimosPorPatrimonio = Array.from(equipamentoCountMap, ([name, value]) => ({ name, value }));
+	}
 }
