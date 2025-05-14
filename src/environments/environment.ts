40c62ec0080f1e75f799af86a3bd0da582707c96
joaosagrath <joaosagrath@hotmail.com>
2025-05-14 13:43:47 -0300

    enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    API: "http://10.35.228.95:8080"
-    //API: "https://easynote.uniamerica.br"
+    //API: "https://10.35.228.117:8443"
+    API: "https://backend.local.easynote.com.br:8444"
+    
+    // API: "http://192.168.0.117:8080"
+    // API: "https://easynote.uniamerica.br"
 };
